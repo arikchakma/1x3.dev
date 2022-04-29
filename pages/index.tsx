d75094730c3a3864876c3cdedb194b45c1151725
Arik Chakma <arikchangma@gmail.com>
2022-04-29 07:51:41 +0600

    Customised animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ const Home: NextPage = () => {
           <motion.div
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.3, ease: [0.075, 0.82, 0.165, 1] }}
+            transition={{ duration: 0.3 }}
             className="grid gap-4"
           >
             <h1 className="font-serif text-3xl font-semibold">
