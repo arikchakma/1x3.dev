b54ac5d15482120f740dbfc5bb34d858e73fb639
Arik Chakma <arikchangma@gmail.com>
2022-04-29 07:49:30 +0600

    Added Animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { motion } from 'framer-motion'
 
 import AvatarGenerate from '../utils/AvatarGenerate'
 
@@ -46,96 +47,119 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex max-w-md flex-col gap-10">
-        <header className="grid gap-4">
-          <h1 className="font-serif text-3xl font-semibold">1X3 Dev Studio</h1>
-          <p className="text-sm text-grayText">
-            A tiny development studio focusing on beautifully crafted visual
-            design for web applications.
-          </p>
-          <div className="flex items-center gap-2 text-sm font-semibold">
-            <div className="h-3 w-3 rounded-md bg-black" />
-            <a
-              href="mailto:hello@arikko.dev?subject=Project%20inquiry!"
-              target="_blank"
-              rel="noreferrer noopener"
-              className="transition-all hover:text-indigo-600"
-            >
-              Next availability: June 2022
-            </a>
-          </div>
+        <header>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, ease: [0.075, 0.82, 0.165, 1] }}
+            className="grid gap-4"
+          >
+            <h1 className="font-serif text-3xl font-semibold">
+              1X3 Dev Studio
+            </h1>
+            <p className="text-sm text-grayText">
+              A tiny development studio focusing on beautifully crafted visual
+              design for web applications.
+            </p>
+            <div className="flex">
+              <a
+                href="mailto:hello@arikko.dev?subject=Project%20inquiry!"
+                target="_blank"
+                rel="noreferrer noopener"
+                className="group transition-all hover:text-indigo-600"
+              >
+                <span className="flex items-center gap-2 text-sm font-semibold">
+                  <span className="h-3 w-3 rounded-md bg-black transition-all group-hover:bg-indigo-600" />
+                  Next availability: June 2022
+                </span>
+              </a>
+            </div>
+          </motion.div>
         </header>
 
         <section>
-          <h2 className="text-xs font-semibold text-grayText">
-            Currently building
-          </h2>
-          <ul className="divide-y divide-black/[0.08]">
-            <li className="py-6">
-              <div className="flex items-center gap-6">
-                <figure className="relative before:absolute before:-z-10 before:h-full before:w-full before:bg-gradient-to-r before:from-[#06F9C4] before:to-[#C406F9] before:blur-lg">
-                  <AvatarGenerate
-                    name="Sentient"
-                    className="h-8 w-8 rounded-2xl"
-                  />
-                </figure>
-                <p className="text-sm">
-                  <span>
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://sentient.dev"
-                      className="font-semibold hover:text-indigo-600"
-                    >
-                      Sentient
-                    </a>
-                  </span>
-                  <span> </span>
-                  <span className="text-grayText">
-                    — Decentralized, DApp, and free.
-                  </span>
-                </p>
-              </div>
-            </li>
-            <li className="py-6 pb-0">
-              <div className="flex items-start gap-6">
-                <figure className="relative before:absolute before:-z-10 before:h-full before:w-full before:bg-gradient-to-r before:from-[#c806f9] before:to-[#f9c806] before:blur-lg">
-                  <AvatarGenerate
-                    name="Sendy"
-                    className="h-8 w-8 rounded-2xl"
-                  />
-                </figure>
-                <p className="text-sm">
-                  <span>
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://sendy.dev"
-                      className="font-semibold hover:text-indigo-600"
-                    >
-                      Sendy
-                    </a>
-                  </span>
-                  <span> </span>
-                  <span className="text-grayText">
-                    — Free, "sent any amount to any account". New features will
-                    be adding every month.
-                  </span>
-                </p>
-              </div>
-            </li>
-          </ul>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.3, ease: [0.075, 0.82, 0.165, 1] }}
+          >
+            <h2 className="text-xs font-semibold text-grayText">
+              Currently building
+            </h2>
+            <ul className="divide-y divide-black/[0.08]">
+              <li className="py-6">
+                <div className="flex items-center gap-6">
+                  <figure className="relative before:absolute before:-z-10 before:h-full before:w-full before:bg-gradient-to-r before:from-[#06F9C4] before:to-[#C406F9] before:blur-lg">
+                    <AvatarGenerate
+                      name="Sentient"
+                      className="h-8 w-8 rounded-2xl"
+                    />
+                  </figure>
+                  <p className="text-sm">
+                    <span>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://sentient.dev"
+                        className="font-semibold hover:text-indigo-600"
+                      >
+                        Sentient
+                      </a>
+                    </span>
+                    <span> </span>
+                    <span className="text-grayText">
+                      — Decentralized, DApp, and free.
+                    </span>
+                  </p>
+                </div>
+              </li>
+              <li className="py-6 pb-0">
+                <div className="flex items-start gap-6">
+                  <figure className="relative before:absolute before:-z-10 before:h-full before:w-full before:bg-gradient-to-r before:from-[#c806f9] before:to-[#f9c806] before:blur-lg">
+                    <AvatarGenerate
+                      name="Sendy"
+                      className="h-8 w-8 rounded-2xl"
+                    />
+                  </figure>
+                  <p className="text-sm">
+                    <span>
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://sendy.dev"
+                        className="font-semibold hover:text-indigo-600"
+                      >
+                        Sendy
+                      </a>
+                    </span>
+                    <span> </span>
+                    <span className="text-grayText">
+                      — Free, "sent any amount to any account". New features
+                      will be adding every month.
+                    </span>
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </motion.div>
         </section>
 
         <footer>
-          <a
-            href="https://twitter.com/ImArikChakma"
-            target="_blank"
-            rel="noreferrer noopener"
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.3, delay: 0.3 * 2, ease: [0.075, 0.82, 0.165, 1] }}
           >
-            <button className="rounded-md bg-black px-3 py-2 text-background transition-all hover:bg-indigo-600 hover:drop-shadow-xl">
-              Let's talk
-            </button>
-          </a>
+            <a
+              href="https://twitter.com/ImArikChakma"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <button className="hover:shadow-inside rounded-md bg-black px-3 py-2 font-medium text-background transition-all duration-200 hover:bg-indigo-600 hover:drop-shadow-lg">
+                Let's talk
+              </button>
+            </a>
+          </motion.div>
         </footer>
       </main>
     </div>
